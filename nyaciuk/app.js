12fbd8261950e121971daa532038d03f98374073
Yaciuk <87411826+Yaciuk@users.noreply.github.com>
2021-07-20 15:55:42 -0400

    var self  = this;

--- nyaciuk/app.js
+++ nyaciuk/app.js
@@ -2,16 +2,19 @@
 
 var c = {
 	name: 'c obj', 
-	log: function() { 
-		this.name= 'Updated c obj';
-		console.log(this); // { "name": "Updated c obj" }  updated c obj
+	log: function() {
+
+		var self  = this;
+
+		self.name= 'Updated c obj';
+		console.log(self); // { "name": "Updated c obj" }  updated c obj
 
 		var setname = function(newname) {
-			this.name = newname;
+			self.name = newname;
 		}
 
 		setname('Update again! The c obj');
-		console.log(this); 	// { "name": "Updated c obj" }  DID NOTHING! WHY?
+		console.log(self); 	// { "name": "Updated c obj" }  DID NOTHING! WHY?
 							// This was added to the global object, window
 	}
 }
