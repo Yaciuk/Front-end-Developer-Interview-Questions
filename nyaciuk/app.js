aafdedd32978e86faa9ae53f4906f839d7a0d13a
Yaciuk <87411826+Yaciuk@users.noreply.github.com>
2021-07-20 15:51:10 -0400

    Bug or Feature me this

--- nyaciuk/app.js
+++ nyaciuk/app.js
@@ -1,29 +1,17 @@
-
-// function statement
-function a() {
-	console.log(this);
-	this.newvar = 'mer';
-}
-
-// function expression
-var b = function(){
-	console.log(this);
-}
-
-// console.log(newvar); // newvar is not defined bc a(); hasn't been invoced
-
-a(); // Window
-
-console.log(newvar); // mer (since a has been invoced) Window.newvar
-
-b(); // Window
-
 // object literal w/ method and this keyword
 
 var c = {
-	name: 'c obj', // property bc primitive
-	log: function() { // method bc function
-		console.log(this); // this refers to c with obj literal/method
+	name: 'c obj', 
+	log: function() { 
+		this.name= 'Updated c obj';
+		console.log(this); // { "name": "Updated c obj" }  updated c obj
+
+		var setname = function(newname) {
+			this.name = newname;
+		}
+
+		setname('Update again! The c obj');
+		console.log(this); // { "name": "Updated c obj" }  DID NOTHING! WHY?
 	}
 }
 
