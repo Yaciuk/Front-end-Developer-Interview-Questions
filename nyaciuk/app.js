347813c909d6ba0aa9bc2b177bff23041eb3e3b0
Yaciuk <87411826+Yaciuk@users.noreply.github.com>
2021-07-20 15:45:05 -0400

    Update app.js

--- nyaciuk/app.js
+++ nyaciuk/app.js
@@ -10,10 +10,21 @@ var b = function(){
 	console.log(this);
 }
 
-console.log(newvar); // mer
+// console.log(newvar); // newvar is not defined bc a(); hasn't been invoced
 
 a(); // Window
 
 console.log(newvar); // mer (since a has been invoced) Window.newvar
 
 b(); // Window
+
+// object literal w/ method and this keyword
+
+var c = {
+	name: 'c obj', // property bc primitive
+	log: function() { // method bc function
+		console.log(this); // this refers to c with obj literal/method
+	}
+}
+
+c.log();
