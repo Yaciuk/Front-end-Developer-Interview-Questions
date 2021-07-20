30dfef24ac3dc10606511c7495e03cf1fb319748
Yaciuk <87411826+Yaciuk@users.noreply.github.com>
2021-07-20 15:59:33 -0400

    End 37 'this' lecture

--- nyaciuk/app.js
+++ nyaciuk/app.js
@@ -4,18 +4,21 @@ var c = {
 	name: 'c obj', 
 	log: function() {
 
+		// Doing this allows
 		var self  = this;
+		// let in ES6 solves this 'this' issue
 
 		self.name= 'Updated c obj';
-		console.log(self); // { "name": "Updated c obj" }  updated c obj
+		console.log(self); 
 
+		// The ability to mutate the correct obj
+		// when function is internal
 		var setname = function(newname) {
 			self.name = newname;
 		}
 
 		setname('Update again! The c obj');
-		console.log(self); 	// { "name": "Updated c obj" }  DID NOTHING! WHY?
-							// This was added to the global object, window
+		console.log(self); 	
 	}
 }
 
