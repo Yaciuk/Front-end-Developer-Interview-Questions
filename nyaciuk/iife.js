31e7ce14f10d15ba2f508c117b8042c07e60c600
Yaciuk <87411826+Yaciuk@users.noreply.github.com>
2021-07-22 14:27:50 -0400

    html temps added

--- nyaciuk/iife.js
+++ nyaciuk/iife.js
@@ -1,12 +1,15 @@
-/* using a function expression with an anonoymous function */
-
-
-var greetFunc = functoin(name){
-	console.log('Hello' + name);
-};
-
-greetFunc('Nicole');
+// Immediately Invoked Function Expression (IIFE)
 
 var iife = function(mer) {
-	console.log('Cat says' + mer);
-}();
+	//console.log('Cat says' + mer);
+
+	return 'Meow' + mer;
+}('Nicole');
+
+
+// wrap funtion in () operator to get function expression
+// So not it assumes you are running this on 'the fly'
+
+(function(mer) {
+	return 'Meow' + mer;
+});
