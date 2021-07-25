20dd5b92eda1e14c510f31a971f1b09d67aaa544
Yaciuk <yaciuknicole@gmail.com>
2021-07-25 04:30:56 -0400

    Create typescript-questions.js

---
+++ src/questions/typescript-questions.js
@@ -0,0 +1,9 @@
+* What is the main advantage of typescript?
+
+Things like creating classes like a transpiled language (the syntax sugar of C++ and the like).
+
+Features: JS evolves quite slowly in terms of browser implementation. If you want to use ES6 features you need to transpile to ES5 anyway. TypeScript 2 is probably ES7+ in many ways. Other languages bring other features. 
+
+// Related to What are some of the advantages/disadvantages of writing JavaScript code in a language that compiles to JavaScript? Advantages: Classes. 
+
+// https://www.markdownguide.org/cheat-sheet/
